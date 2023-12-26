42e68b2f503544a68d872b94cf2d04d47d33ebca
Imperor_Ivan <ivan.dremach@gmail.com>
2023-12-26 23:12:58 +0300

    добавлено рисование прямоугольников, начальный экран и автосохранение холста

--- project.py
+++ project.py
@@ -12,9 +12,12 @@ def paint():
     running = True
     flag_ris = True
     flag_lastika = False
+    flag_pryam = False
+    flag = True
     nazhat = False
     rad = 4
-    zvet = pygame.Color('RED')
+    zvet = pygame.Color('black')
+    zvet_pryam = pygame.Color('red')
     zvet_lastika = pygame.Color('white')
     previous = pygame.Color('white')
     fon = False
@@ -22,10 +25,15 @@ def paint():
     openfile = True
     prev = False
     coords = ()
+    history = []
+    svezh = [[]]
+    nach = True
+    flag = False
+    i_global = 0
 
     white = True
-    black = True
-    red = False
+    black = False
+    red = True
     orange = True
     yellow = True
     green = True
@@ -34,6 +42,9 @@ def paint():
 
     def baza():
         f1 = pygame.font.Font(None, 24)
+        if flag:
+            back = pygame.image.load('./Безымянный.png')
+            screen.blit(back, (0, 0))
         if not fon:
             text1 = f1.render('Выберите цвет кисти:', True, (180, 0, 0))
         else:
@@ -43,6 +54,7 @@ def paint():
         clear = pygame.image.load('./data/clear.png')
         zalivka = pygame.image.load('./data/zalivka.png')
         papka = pygame.image.load('./data/papka.png')
+        kvadrat = pygame.image.load('./data/kvadrat.png')
         if flag_lastika:
             pygame.draw.rect(screen, pygame.Color('gray'), [10, 10, 30, 30], )
             screen.blit(eraser, (10, 10))
@@ -50,6 +62,10 @@ def paint():
         elif flag_ris:
             pygame.draw.rect(screen, pygame.Color('gray'), [10, 10, 30, 30], )
             screen.blit(brush, (10, 10))
+            pygame.display.update()
+        elif flag_pryam:
+            pygame.draw.rect(screen, pygame.Color('gray'), [10, 10, 30, 30], )
+            screen.blit(kvadrat, (10, 10))
             pygame.display.update()
         if fon:
             pygame.draw.rect(screen, pygame.Color('blue'), [125, 5, 40, 40], )
@@ -105,28 +121,41 @@ def paint():
             pygame.display.update()
     while running:
         # Отрисовка интерфейса
-        baza()
+        if nach:
+            baza()
+        if flag:
+            bg = pygame.image.load('./Безымянный.png')
+            screen.blit(bg, (0, 0))
 
         for event in pygame.event.get():
             # Выход из программы
             if event.type == pygame.QUIT:
+                start()
                 running = False
 
             # Проверка на нажатие
             if event.type == pygame.MOUSEBUTTONDOWN and event.pos[1] >= 50:
                 nazhat = True
                 coords = event.pos
+                if flag_ris:
+                    svezh[0].append(zvet)
+                    svezh.append([])
+                    i_global += 1
 
             # Смена режима ластик/кисть
-            if event.type == pygame.MOUSEBUTTONDOWN and 10 <= event.pos[0] <= 39 \
-                    and 10 <= event.pos[1] <= 39 and flag_ris:
-                flag_ris = False
-                flag_lastika = True
-
-            elif event.type == pygame.MOUSEBUTTONDOWN and 10 <= event.pos[0] <= 39 \
-                    and 10 <= event.pos[1] <= 39 and flag_lastika:
-                flag_ris = True
-                flag_lastika = False
+            if event.type == pygame.MOUSEBUTTONDOWN and 10 <= event.pos[0] <= 39 and 10 <= event.pos[1] <= 39:
+                if flag_ris:
+                    flag_ris = False
+                    flag_lastika = True
+                    flag_pryam = False
+                elif flag_lastika:
+                    flag_ris = False
+                    flag_lastika = False
+                    flag_pryam = True
+                elif flag_pryam:
+                    flag_pryam = False
+                    flag_ris = True
+                    flag_lastika = False
 
             # Смена режима цвет кисти/цвет фона
             if event.type == pygame.MOUSEBUTTONDOWN and 130 <= event.pos[0] <= 159 \
@@ -258,6 +287,9 @@ def paint():
                     and 10 <= event.pos[1] <= 39:
                 screen.fill(zvet_lastika)
                 baza()
+                history = []
+                svezh = [[]]
+                i_global = 0
                 pygame.display.flip()
 
             # Задний фон
@@ -266,15 +298,6 @@ def paint():
                 screen.fill(pygame.Color('white'))
                 baza()
                 zvet_lastika = pygame.Color('white')
-                white = True
-                black = False
-                red = False
-                orange = False
-                yellow = False
-                green = False
-                blue = False
-                purple = False
-                fon = False
                 pygame.display.flip()
             if event.type == pygame.MOUSEBUTTONDOWN and 570 <= event.pos[0] <= 599\
                     and 10 <= event.pos[1] <= 39 and fon and zvet_lastika != pygame.Color('black'):
@@ -326,23 +349,89 @@ def paint():
                 zvet_lastika = pygame.Color('purple')
                 fon = False
                 pygame.display.flip()
+
             # Проверка на отжатие мыши
             if event.type == pygame.MOUSEBUTTONUP:
                 nazhat = False
+                pygame.image.save(screen, 'Безымянный.png')
+                if flag_pryam:
+                    if event.pos[1] >= 50:
+                        if coords[0] <= pygame.mouse.get_pos()[0] and coords[1] <= pygame.mouse.get_pos()[1]:
+                            a, b = abs(coords[0]-pygame.mouse.get_pos()[0]), abs(coords[1]-pygame.mouse.get_pos()[1])
+                            x, y = coords
+                            history.append(((x, y), (a, b)))
+                            # print(history)
+                        elif coords[0] >= pygame.mouse.get_pos()[0] and coords[1] >= pygame.mouse.get_pos()[1]:
+                            a, b = abs(coords[0]-pygame.mouse.get_pos()[0]), abs(coords[1]-pygame.mouse.get_pos()[1])
+                            x, y = pygame.mouse.get_pos()
+                            history.append(((x, y), (a, b)))
+                            # print(history)
+                        elif coords[0] <= pygame.mouse.get_pos()[0] and coords[1] >= pygame.mouse.get_pos()[1]:
+                            a, b = abs(coords[0]-pygame.mouse.get_pos()[0]), abs(coords[1]-pygame.mouse.get_pos()[1])
+                            x, y = coords[0], coords[1] - b
+                            history.append(((x, y), (a, b)))
+                            # print(history)
+                        elif coords[0] >= pygame.mouse.get_pos()[0] and coords[1] <= pygame.mouse.get_pos()[1]:
+                            a, b = abs(coords[0]-pygame.mouse.get_pos()[0]), abs(coords[1]-pygame.mouse.get_pos()[1])
+                            x, y = coords[0] - a, coords[1]
+                            history.append(((x, y), (a, b)))
+                            # print(history)
+
 
         # Рисование кистью
         if flag_ris and nazhat:
+            svezh[i_global].append(coords)
             pygame.draw.circle(screen, zvet, pygame.mouse.get_pos(), rad)
             pygame.draw.line(screen, zvet, coords, pygame.mouse.get_pos(), 3 * rad)
             coords = pygame.mouse.get_pos()
             pygame.display.update()
 
         # Стирание ластиком
-        elif flag_lastika and nazhat:
+        if flag_lastika and nazhat:
             pygame.draw.circle(screen, zvet_lastika, pygame.mouse.get_pos(), rad)
             pygame.draw.line(screen, zvet_lastika, coords, pygame.mouse.get_pos(), 3 * rad)
             coords = pygame.mouse.get_pos()
             pygame.display.update()
+
+        # Рисование прямоугольника
+        if flag_pryam and nazhat:
+            pygame.display.flip()
+            # 4 четверть
+            if coords[0] <= pygame.mouse.get_pos()[0] and coords[1] <= pygame.mouse.get_pos()[1]:
+                a, b = abs(coords[0] - pygame.mouse.get_pos()[0]), abs(coords[1] - pygame.mouse.get_pos()[1])
+                x, y = coords
+                # pygame.draw.rect(screen, zvet_lastika, [0, 50, width, height])
+                bg = pygame.image.load('./Безымянный.png')
+                screen.blit(bg, (0, 0))
+                pygame.draw.rect(screen, zvet_pryam, [x, y,  a, b], 2)
+                pygame.display.update()
+            # 2 четверть
+            elif coords[0] >= pygame.mouse.get_pos()[0] and coords[1] >= pygame.mouse.get_pos()[1]:
+                a, b = abs(coords[0] - pygame.mouse.get_pos()[0]), abs(coords[1] - pygame.mouse.get_pos()[1])
+                x, y = pygame.mouse.get_pos()
+                # pygame.draw.rect(screen, zvet_lastika, [0, 50, width, height])
+                bg = pygame.image.load('./Безымянный.png')
+                screen.blit(bg, (0, 0))
+                pygame.draw.rect(screen, zvet_pryam, [x, y, a, b], 2)
+                pygame.display.update()
+            # 1 четверть
+            elif coords[0] <= pygame.mouse.get_pos()[0] and coords[1] >= pygame.mouse.get_pos()[1]:
+                a, b = abs(coords[0] - pygame.mouse.get_pos()[0]), abs(coords[1] - pygame.mouse.get_pos()[1])
+                x, y = coords[0], coords[1] - b
+                # pygame.draw.rect(screen, zvet_lastika, [0, 50, width, height])
+                bg = pygame.image.load('./Безымянный.png')
+                screen.blit(bg, (0, 0))
+                pygame.draw.rect(screen, zvet_pryam, [x, y, a, b], 2)
+                pygame.display.update()
+            # 3 четверть
+            elif coords[0] >= pygame.mouse.get_pos()[0] and coords[1] <= pygame.mouse.get_pos()[1]:
+                a, b = abs(coords[0] - pygame.mouse.get_pos()[0]), abs(coords[1] - pygame.mouse.get_pos()[1])
+                x, y = coords[0] - a, coords[1]
+                # pygame.draw.rect(screen, zvet_lastika, [0, 50, width, height])
+                bg = pygame.image.load('./Безымянный.png')
+                screen.blit(bg, (0, 0))
+                pygame.draw.rect(screen, zvet_pryam, [x, y, a, b], 2)
+                pygame.display.update()
 
         # Отсутствие лишней прорисовки
         if prev:
@@ -350,4 +439,35 @@ def paint():
             prev = False
 
 
-paint()
+def start():
+    pygame.init()
+    width = 800
+    height = 600
+    screen = pygame.display.set_mode([width, height])
+    screen.fill(pygame.Color('white'))
+    pygame.display.flip()
+    running = True
+
+    def osnova():
+        screen.fill(pygame.Color('white'))
+        pygame.draw.rect(screen, pygame.Color('purple'), [0, 0, 400, 600])
+        pygame.draw.rect(screen, pygame.Color('green'), [400, 0, 400, 600])
+        f1 = pygame.font.Font(None, 100)
+        text1 = f1.render('PAINT', True, (180, 0, 0))
+        screen.blit(text1, (100, 250))
+        text2 = f1.render('ДРУГОЕ', True, (180, 0, 0))
+        screen.blit(text2, (450, 250))
+
+        pygame.display.flip()
+    while running:
+        osnova()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] < 400:
+                paint()
+                running = False
+
+
+start()
+# paint()
